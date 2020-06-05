f39528eb60298b54991bd12c68a1c8fbc99cd0ee
Jerome Ko <jerome@circles.asia>
2020-06-05 21:41:49 +0800

    Built movies rec func

--- telegram/telegram_bot.py
+++ telegram/telegram_bot.py
@@ -6,19 +6,29 @@ import googlemaps
 from geopy.geocoders import Nominatim
 import pprint
 import random
+from tmdbv3api import TMDb, Movie
 
 # Argument check
-if len(sys.argv) != 3:
-    print("Please parse exactly 3 arguments")
+if len(sys.argv) != 4:
+    print("Please parse exactly 4 arguments")
     sys.exit(1)
 
 # Import your API keys here
 g_api_key = sys.argv[1]
 t_token = sys.argv[2]
+tmdb_api_key = sys.argv[3]
 
-# # Define gmaps Client
+# Define gmaps Client
 gmaps = googlemaps.Client(key = g_api_key)
 geolocator = Nominatim(user_agent=g_api_key)
+
+# Define tmdb Client
+tmdb = TMDb()
+tmdb.api_key = tmdb_api_key
+
+# Configuration settings
+tmdb.language = 'en'
+tmdb.debug = True
 
 # Telegram codes
 # Start telebot
@@ -28,12 +38,13 @@ def main():
     # Add event handler that returns start() function when user inputs /start or /help in chat
     @bot.message_handler(commands=["start", "help"])
     def send_welcome(message):
-        bot.reply_to(message, """Recommends food/drink places for lazy and hungry souls :) 
+        bot.reply_to(message, """Recommends movies, food/drink places for lazy and hungry souls :) 
         Type /foodnearby to find food recs near your entered location
         Type /drinksnearby to find pub recs near your entered location
-        Type /randomfood to find food recs randomly""")
+        Type /randomfood to find food recs randomly
+        Type /trendingmovies to find trending movie recs""")
 
-    # Activate the geolocator only with /foodnearby or /drinksnearby command so we don't spam the google API
+    # Activates the geolocator only with /foodnearby or /drinksnearby command so we don't spam the google API
     @bot.message_handler(commands=["foodnearby"])
     def food(message):
         msg = bot.reply_to(message, "Let me know where you are!")
@@ -53,7 +64,7 @@ def main():
 
         # If google API cannot return a result
         except:
-            bot.reply_to(message, "Please make a valid request")
+            bot.reply_to(message, "Please make a valid request.")
 
     
     @bot.message_handler(commands=["drinksnearby"])
@@ -75,9 +86,9 @@ def main():
 
         # If google API cannot return a result
         except:
-            bot.reply_to(message, "Please make a valid request")
+            bot.reply_to(message, "Please make a valid request.")
 
-    # Randomly suggest 5 places when user keys in /random command. Search starts from central (i.e. AMK) area
+    # Randomly suggests 5 places when user keys in /random command. Search starts from central (i.e. AMK) area
     @bot.message_handler(commands=["randomfood"])
     def randomfood_place_message(message):
         try:
@@ -90,7 +101,20 @@ def main():
 
         # If google API cannot return a result
         except:
-            bot.reply_to(message, "Please make a valid request")
+            bot.reply_to(message, "Please make a valid request.")
+
+    # Randomly suggests top trending movies
+    @bot.message_handler(commands=["trendingmovies"])
+    def movie_message(message):
+        try:
+            movie = Movie()
+            popular = movie.popular()
+
+            # Reply message with recs
+            bot.reply_to(message, message_output_shows(show_details(popular)))
+        
+        except:
+            bot.reply_to(message, "Service is down. Please try again later.")
 
     ######################
     # Function definitions
@@ -106,7 +130,7 @@ def main():
         places_results = gmaps.places_nearby(location = geoarea, radius = radiusspread, open_now = True, type = category)["results"]
 
         # We can extract name, rating, user_ratings_total, price_level, and vicinity from above using list comprehension
-        # Exclude those with missing keys
+        # Excludes those with missing keys
         # Using random.sample to randomly pick up 5 from list
         place_details = [{"name": item["name"], 
                         "rating": item["rating"], 
@@ -135,11 +159,34 @@ def main():
         # Returns joined items from the list, in the form of a string
         return "\n\n".join(output)
 
+
+    # Extracts show details
+    def show_details(shows):
+        top_shows = [{"title": s.title,
+            "year": s.release_date,
+            "popularity": s.popularity,
+            "rating": s.vote_average,
+            "summary": s.overview}
+            for s in shows
+            if s.vote_average >= 6.2]
+        
+        return random.sample(top_shows, 5)
+
+    # Converts show details from list to string
+    def message_output_shows(shows):
+        output = []
+        for show in shows:
+            title = show["title"]
+            year = show["year"]
+            popularity = show["popularity"]
+            rating = show["rating"]
+            summary = show["summary"]
+            output.append(f"Title: {title} | Year: {year} | Popularity: {popularity} | Rating: {rating} | Summary: {summary}")
+        
+        # Returns joined items from the list, in the form of a string
+        return "\n\n".join(output)
+
     # Start the bot
     bot.polling()
 
 main()
-
-# Save visited places in a DB
-
-# Can pull out favorites places from DB
